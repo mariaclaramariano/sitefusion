6c4c578875e018115d4f2c1149ff4689f743f999
mariaclaramariano <113188261+mariaclaramariano@users.noreply.github.com>
2023-08-27 02:35:07 -0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.views.generic import FormView
 from .forms import ContatoForms
 from django.urls import reverse_lazy
-from .models import Servico, Equipe, features
+from .models import servico, Equipe, features
 from  django.contrib import messages
 
 class indexview(FormView):
@@ -13,7 +13,7 @@ class indexview(FormView):
 
     def get_context_data(self, **kwargs):
         context = super(indexview, self).get_context_data(**kwargs)
-        context ['Servicos']= Servico.objects.all()
+        context ['Servicos']= servico.objects.all()
         context['equipes'] = Equipe.objects.all()
         context['features']= features.objects.all()
         return context
@@ -21,10 +21,10 @@ class indexview(FormView):
     def form_valid(self, form, *args, **kwargs ):
         form.send_email()
         messages.success(self.request, 'email enviado com sucesso')
-        return super(indexview, self).form_valid(form, *args, **kwargs)
+        return super(indexView, self).form_valid(form, *args, **kwargs)
 
     def form_invalid(self, form, *args, **kwargs):
         messages.error(self.request, 'erro ao enviar!')
-        return super(indexview, self).form_invalid(form, *args, **kwargs)
+        return super(indexView, self).form_invalid(form, *args, **kwargs)
         
         
